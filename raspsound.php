c45d3aa9fce6a36dafe30ef192a738adf3407249
quochuy2204 <quochuy2204@outlook.com>
2017-03-24 11:10:20 +0000

    third commit

---
+++ raspsound.php
@@ -0,0 +1,14 @@
+﻿<?php
+$data = $_POST['data'];
+
+$today = date("Y-m-d H:i:s");	//timestamp of this reception
+$record = $today . ",  " . $data."\n";
+
+$file = "sound.log";	//specify the file name
+	if(file_exists($file))
+		$fp = fopen($file, "a");	//open file in appanding mode
+	else
+		$fp = fopen($file, "w");	//open file in wrting mode
+	fwrite($fp, $record);			//write the record to the file
+	fclose($fp);
+?>
