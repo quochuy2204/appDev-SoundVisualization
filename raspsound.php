5ad045d12ec93dbb73c9d3dacf90fe94a494c6d5
quochuy2204 <quochuy2204@outlook.com>
2017-03-24 13:11:41 +0200

    Update raspsound.php

--- raspsound.php
+++ raspsound.php
@@ -2,7 +2,7 @@
 $data = $_POST['data'];
 
 $today = date("Y-m-d H:i:s");	//timestamp of this reception
-$record = $today . ",  " . $data."\n";
+$record = $today . ", " . $data."\n";
 
 $file = "sound.log";	//specify the file name
 	if(file_exists($file))
