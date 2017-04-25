6cf66c72c8c7f09a7eed4f28954823a48f6fcac2
quochuy2204 <quochuy2204@outlook.com>
2017-04-25 11:51:01 +0300

    Add files via upload

--- raspsound.php
+++ raspsound.php
@@ -2,9 +2,9 @@
 $data = $_POST['data'];
 
 $today = date("Y-m-d H:i:s");	//timestamp of this reception
-$record = $today . ", " . $data."\n";
+$record = $data."\n";
 
-$file = "sound.log";	//specify the file name
+$file = "soundlog.txt";	//specify the file name
 	if(file_exists($file))
 		$fp = fopen($file, "a");	//open file in appanding mode
 	else
